cac536667fb7fd4e605ed89504553d4dd504666c
MrBoolean <marcandrebinder@gmail.com>
2015-10-12 12:51:39 +0200

    Add codeclimate task

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var eslint = require('gulp-eslint');
 var istanbul = require('gulp-istanbul');
 var mocha = require('gulp-mocha');
-// var codeclimate = require('gulp-codeclimate-reporter');
+var codeclimate = require('gulp-codeclimate-reporter');
 
 gulp.task('test.instrument', function instrument() {
   return gulp
@@ -32,6 +32,15 @@ gulp.task('lint', function lint() {
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError())
+  ;
+});
+
+gulp.task('codeclimate', function sendToCodeclimate() {
+  return gulp
+    .src(['dist/report/lcov.info'], { read: false })
+    .pipe(codeclimate({
+      token: 'f33865ded01b9dbd0f27e2b9eaed53e9e8d8e8396fea403476c03f079021dc9e'
+    }))
   ;
 });
 
